e5e104c3d10c8ae816de212285833210219b88c8
Feeeeeh <klitzketech@gmail.com>
2026-03-03 21:03:11 -0300

    chore: init

---
+++ adicao/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bem vindo(a) a calculadora de soma\r\nInsira o primeiro numero");
+        int primeiro = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("insira o segundo numero");
+        int segundo = int.Parse(Console.ReadLine());
+        int resultado = primeiro + segundo;
+        Console.WriteLine("Resultado: " + resultado);
+    }
+}
---
+++ formulario/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+preenchimento de formulário simples
+dicionario com as informações organizadas
+nome idade peso altura 
+conseguir chamar a informação por nome
+*/
+
+internal class Program
+
+{
+    private static void Main()
+    {
+        var usuarios = new Dictionary<string, string>();
+        Console.WriteLine("Insira seu nome: ");
+        string nome = Console.ReadLine();
+        usuarios.Add("nome", nome);
+        foreach (var i in usuarios)
+        {
+            Console.WriteLine($"nome: {i.Value}");
+        }
+    }
+}
