d5468cd5125120eea436e97e00087dd101a8e7bf
Felipe Klitzke de Godoi <klitzketech@gmail.com>
2026-03-10 20:52:54 -0300

    Add files via upload

---
+++ numero extenso/Program.cs
@@ -0,0 +1,25 @@
+﻿var dicExtenso = new Dictionary<int, string>
+{
+    {1,"Um"},
+    {2,"Dois"},
+    {3,"Três"},
+    {4,"Quatro"},
+    {5,"Cinco"},
+    {6,"Seis"},
+    {7,"Sete"},
+    {8,"Oito"},
+    {9,"Nove"},
+    {10,"Dez"}
+};
+
+Console.WriteLine("Digite um número entre 1 e 10 para recebe-lo por extenso");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if (dicExtenso.TryGetValue(numero, out string? extenso))
+{
+    Console.WriteLine($"A forma extensa do número {numero} é {extenso}");
+}
+else
+{
+    Console.WriteLine("Número inválido");
+}
