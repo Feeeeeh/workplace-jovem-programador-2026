de9ee55aa8deddb6df3eeb0674e0c94ff244b685
Felipe Klitzke de Godoi <klitzketech@gmail.com>
2026-03-10 21:49:13 -0300

    Add files via upload

---
+++ pokemon/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine(@"Escolha seu Pokémon inicial!
+[1] Charmander 
+[2] Bulbasauro
+[3] Squirtle
+            ");
+int escolha = Convert.ToInt32(Console.ReadLine());
+
+switch (escolha)
+{
+    case 1:
+    Console.WriteLine("Você escolheu Charmander!");
+    break;
+
+    case 2:
+    Console.WriteLine("Você escolheu Bulbasauro!");
+    break;
+
+    case 3:
+    Console.WriteLine("Você escolheu Squirtle!");
+    break;
+
+    case 4:
+    Console.WriteLine("Você descobriu a escolha secreta e conseguiu um Pikachu!");
+    break;
+
+    default:
+    Console.WriteLine("Escolha inválida, vai ficar sem Pokémon.");
+    break;
+}
